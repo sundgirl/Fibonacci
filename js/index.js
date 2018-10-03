4b1d589beb5e9b0f35c457c954314783f3716b71
Diana <sundgirl19@gmail.com>
2018-10-03 13:25:57 +0000

    Fibo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const number = parseInt(prompt('Enter a number'));
 
 function solFibRec(n) {
-    if (n==0 || n==1)
+    if (n==1 || n==2)
         return 1;
     else
         return solFibRec(n-1)+solFibRec(n-2);
@@ -12,7 +12,7 @@ function solFibCycle(n) {
     let firstNumber = 1;
     let secondNumber = 1;
     let temp = 0;
-    for (let i=1; i<n; i++) {
+    for (let i=2; i<n; i++) {
         temp = secondNumber;
         secondNumber += firstNumber;
         firstNumber = temp;
@@ -24,7 +24,7 @@ alert("Циклом зі змінними = " + solFibCycle(number));
 function solFibArr(n) {
     let arr = [];
     arr[0] = 1; arr[1] = 1;
-    for (let i=2; i<=n; i++){
+    for (let i=2; i<n; i++){
         arr[i] = arr[i-1] + arr[i-2];
     }
     return arr.pop();
